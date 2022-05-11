3e55f4080f245b823cc2af627f80c4952c4586d4
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-05-11 21:39:52 +0530

    home page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+import requests, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pokemons = requests.get("https://studiousapi.up.railway.app/pokedata/data/random?limit=20").json()
+    return render_template('index.html', pokemons=pokemons)
 
 if __name__ == '__main__':
     app.run(debug=True)
