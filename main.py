63db840f86ad9bdd90c568332ae4048b13f9492c
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-05-11 21:53:44 +0530

    searching

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ def index():
     pokemons = requests.get("https://studiousapi.up.railway.app/pokedata/data/random?limit=20").json()
     return render_template('index.html', pokemons=pokemons)
 
+@app.route('/search')
+def search_():
+    return render_template('search.html', pokemons=None, query="")
+
+@app.route('/search/<query>')
+def search(query):
+    result = requests.get(f"https://studiousapi.up.railway.app/pokedata/data/search?query={query}").json()
+    return render_template('search.html', pokemons=result, query=query)
+
 if __name__ == '__main__':
     app.run(debug=True)
