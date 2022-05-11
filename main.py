327d117a44c8f90cca2c453f06156d5128d81370
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-05-11 21:58:30 +0530

    ready to deploy

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def search(query):
     return render_template('search.html', pokemons=result, query=query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
